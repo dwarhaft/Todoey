f162528dbc6fa9519aeca87aea05ef9e6d931279
David Warhaft <davidwarhaft@davids-air.hitronhub.home>
2018-03-27 15:37:49 -0600

    convert to MVC design pattern,

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,18 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by David Warhaft on 2018-03-23.
+//  Copyright © 2018 David Warhaft. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var  title: String = ""
+    var  done: Bool = false
+    
+    
+    
+}
